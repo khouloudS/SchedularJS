0a37c65667ad101b1ecb556124a31fe0f77195b8
khouloud.sellami <46864709+khouloudS@users.noreply.github.com>
2021-04-04 13:18:24 +0200

    [FEAT] - Event: Add busy slots.

--- Event/event.js
+++ Event/event.js
@@ -1,5 +1,6 @@
 var eventList = [];
 var FreeEvents = [];
+var BusyEvents = [];
 const NEXT_WEEK=7*24*60*60*1000; //same time next week in mill sec
 const NEXT_SLOT=30*60*1000; //next 30 min in mill sec
 
@@ -20,6 +21,14 @@ Event.prototype.availabilities = function (fromDate, toDate) {
             AddFreeEvents(element, Date.parse(fromDate), Date.parse(toDate), start_open, end_open);
         };
     });
+    eventList.forEach(function (element) {
+        if (element.opening === false) {
+            //  get busy windows (start & end) in mill sec
+            start_busy = Date.parse(element.startDate);
+            end_busy = Date.parse(element.endDate);
+            AddBusyEvents(element, Date.parse(fromDate), Date.parse(toDate), start_busy, end_busy);
+        };
+    });
 };
 
 function AddFreeEvents(element, fromDate, toDate, start_open, end_open) {
@@ -33,6 +42,20 @@ function AddFreeEvents(element, fromDate, toDate, start_open, end_open) {
     //  recursive call for recurring available events
     if (element.recurring === true && start_open < toDate) {
         AddFreeEvents(element, fromDate, toDate, start_open + NEXT_WEEK, end_open + NEXT_WEEK);
+    }
+}
+function AddBusyEvents(element, fromDate, toDate, start_busy, end_busy) {
+    start_date=Math.max(fromDate,start_busy);
+    end_date=Math.min(toDate,end_busy);
+    next_slot = start_date;
+    while (next_slot < end_date) {
+        BusyEvents.push(timeToString(next_slot));
+        next_slot += NEXT_SLOT; // check next 30 min slot
+    };
+
+    //  recursive call for recurring busy events
+    if (element.recurring === true && start_busy < toDate) {
+        AddBusyEvents(element, fromDate, toDate, start_busy + NEXT_WEEK, end_busy + NEXT_WEEK);
     }
 }
 
