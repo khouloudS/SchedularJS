b124903f7fe576224e3abebcca2ceb0a1befbaf8
khouloud.sellami <46864709+khouloudS@users.noreply.github.com>
2021-04-04 13:16:15 +0200

    [FEAT] - Event: Add free slots.

--- Event/event.js
+++ Event/event.js
@@ -1,17 +1,46 @@
 var eventList = [];
+var FreeEvents = [];
+const NEXT_WEEK=7*24*60*60*1000; //same time next week in mill sec
+const NEXT_SLOT=30*60*1000; //next 30 min in mill sec
 
-var Event = function(opening, recurring, startDate, endDate){
+var Event = function (opening, recurring, startDate, endDate) {
     this.opening = opening;
     this.recurring = recurring;
     this.startDate = startDate;
     this.endDate = endDate;
-
     eventList.push(this);
 };
 
-Event.prototype.availabilities = function(fromDate, toDate){
-    return //Something awesome;
+Event.prototype.availabilities = function (fromDate, toDate) {
+    eventList.forEach(function (element) {
+        if (element.opening === true) {
+            //  get free slot (start & end) in mill sec
+            start_open = Date.parse(element.startDate);
+            end_open = Date.parse(element.endDate);
+            AddFreeEvents(element, Date.parse(fromDate), Date.parse(toDate), start_open, end_open);
+        };
+    });
 };
 
-module.exports = Event;
+function AddFreeEvents(element, fromDate, toDate, start_open, end_open) {
+    start_date=Math.max(fromDate,start_open);
+    end_date=Math.min(toDate,end_open);
+    next_slot = start_date;
+    while (next_slot < end_date){
+        FreeEvents.push(timeToString(next_slot));
+        next_slot += NEXT_SLOT; // check next 30 min slot
+    };
+    //  recursive call for recurring available events
+    if (element.recurring === true && start_open < toDate) {
+        AddFreeEvents(element, fromDate, toDate, start_open + NEXT_WEEK, end_open + NEXT_WEEK);
+    }
+}
 
+function timeToString(time_mill) {
+    var date_time = new Date(time_mill);
+    return  date_time.toString().split(' ')[1]+ ' '+ date_time.getDate()+  ' at ' + date_time.getHours() + ':' + date_time.getMinutes();
+}
+
+
+
+module.exports = Event;
